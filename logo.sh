1fbbc3ebc057ab91606fb20cb75c3ff14dac03f5
OfficialDarkAngle <67592435+OfficialDarkAngle@users.noreply.github.com>
2020-06-29 21:20:24 +0700

    Add files via upload

---
+++ logo.sh
@@ -0,0 +1,25 @@
+green="\033[92;1m"
+yellow="\033[93;1m"
+indigo="\033[94;1m"
+red="\033[91;1m"
+magenta="\033[95;1m"
+cyan="\033[96;1m"
+white="\033[97;1m"
+blue="\033[94;1m"
+clear
+sleep 0.5
+echo  $green "        ╭──────────•◈•──────────╮"
+echo  $red "           ╔═╗┌─┐┌─┐┬┌─┐┬┌─┐┬    "
+echo  $red "           ║ ║├─ ├─ ││  │├─┤│    "$yellow      "V 0.1"
+echo  $red "           ╚═╝┴  ┴  ┴┴─┘┴┴ ┴┴─┘  "
+sleep 0.5
+echo  $red "                 ╦┌─┐┬─┐┬        "$red      "      ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓"
+echo  $red "               ╔═║├─┤├┬┘│〲      "$red      "      ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓"
+echo  $white "               ╚═╝┴ ┴┴└─┴        "$white      "      ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓"
+sleep 0.5
+echo  $white "             ╔═╗┬─┐┌─┐┬  ┌─┐    "$white      "       ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓"
+echo  $white "             ╠═╣│ ││ ┐│  ├─     "
+echo  $white "             ╩ ╩┴ └┴─┘┴─┘┴─┘    "
+echo  $green "        ╰──────────•◈•──────────╯"
+echo''
+
